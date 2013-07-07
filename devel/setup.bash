a4ad638115dfe3f8229991730507ec5338656c63
MLefebvre <mathieuglefebvre@gmail.com>
2013-07-07 13:10:48 -0400

    Initial commit

---
+++ devel/setup.bash
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# generated from catkin/cmake/templates/setup.bash.in
+
+CATKIN_SHELL=bash
+. "/home/mathieu/ibex/devel/setup.sh"
---
+++ devel/setup.zsh
@@ -0,0 +1,7 @@
+#!/usr/bin/env zsh
+# generated from catkin/cmake/templates/setup.zsh.in
+
+CATKIN_SHELL=zsh
+emulate sh # emulate POSIX
+. "/home/mathieu/ibex/devel/setup.sh"
+emulate zsh # back to zsh mode
