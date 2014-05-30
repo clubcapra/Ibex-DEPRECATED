f4a74c2d1aae549539c5a26b2c6cbbdea3f1bffb
MLefebvre <mathieuglefebvre@gmail.com>
2014-05-29 20:02:47 -0400

    Modification du setup.sh pour les changements de rosjava

--- setup.sh
+++ setup.sh
@@ -1,16 +1,26 @@
-git submodule init  
-git submodule update
-git submodule update --init --recursive
-catkin_make
-source devel/setup.bash
+#git submodule init  
+#git submodule update
+#git submodule update --init --recursive
+#catkin_make
+#source devel/setup.bash
 
 cwd="$PWD"
-for i in src/capra_*
-do
+# Pour tous les packages rosjava
+for i in src/capra_*; do
   if [ -e "$i/gradlew" ] && [ "$i" != "src/capra_msgs_java" ]; then
     cd $i
     git checkout master
-    ../gradlew deployApp
+    # Pour les sous-projets
+    for j in *; do
+        if [ -d "${j}" ]; then
+          if [ -e "$j/build.gradle" ]; then
+              cd $j
+              echo "$PWD"
+              ../gradlew deployApp
+              cd ..
+          fi
+        fi
+    done
     cd $cwd
   fi
 done
