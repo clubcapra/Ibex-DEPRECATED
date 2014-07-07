1e27792678d35f37bc44eb0de82046f51b197f9b
Mathieu Gascon-Lefebvre <mathieuglefebvre@gmail.com>
2014-07-07 19:51:55 -0400

    Déplacement de smart motor

--- src/animatics_smart_motor/scripts/includes/commands/command.py
+++
@@ -1,8 +0,0 @@
-class Command(object):
-    response_type = None
-    
-    def __init__(self, response_type=None):
-        self.response_type = response_type
-
-    def __str__(self):
-        return ""
