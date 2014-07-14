acc97685df0e2800fd536708371cfb0055eac266
= <capra@ens.etsmtl.ca>
2014-07-14 17:34:08 -0400

    Modified master.sh script to use rospack

--- scripts/master.sh
+++ scripts/master.sh
@@ -1,3 +1,3 @@
 export ROS_IP=192.168.1.10
 sudo ifconfig wlan0 192.168.1.10 netmask 255.255.255.0 up
-roslaunch ../src/capra_2dnav/launch/capra_configuration.launch outdoor:=0
+roslaunch $(rospack find capra_2dnav)/launch/capra_configuration.launch outdoor:=0
