c7e867c335eead789310aa055c5d729a0023fef9
= <capra@ens.etsmtl.ca>
2014-07-14 16:58:00 -0400

    Added script to use remotely

--- setup.sh
+++ setup.sh
@@ -6,23 +6,23 @@ source devel/setup.bash
 
 cwd="$PWD"
 
-for i in `grep path .gitmodules | sed 's/.*= //'`; do
-    cd $i
-#    git checkout master
-    # Si c'est un package rosjava
-    if [ -e "gradlew" ]; then
-        # Pour tous les sous-projets
-        for j in *; do
-            if [ -d "${j}" ]; then
-                if [ -e "$j/build.gradle" ] && [ "$j" != "capra_msgs" ]; then
-                    cd $j
-                    echo $j
-                    ../gradlew deployApp
-                    cd ..
-                fi
-            fi
-        done
-    fi
-    cd $cwd
-done
+#for i in `grep path .gitmodules | sed 's/.*= //'`; do
+#    cd $i
+##    git checkout master
+#    # Si c'est un package rosjava
+#    if [ -e "gradlew" ]; then
+#        # Pour tous les sous-projets
+#        for j in *; do
+#            if [ -d "${j}" ]; then
+#                if [ -e "$j/build.gradle" ] && [ "$j" != "capra_msgs" ]; then
+#                    cd $j
+#                    echo $j
+#                    ../gradlew deployApp
+#                    cd ..
+#                fi
+#            fi
+#        done
+#    fi
+#    cd $cwd
+#done
 
