cc9e8d53ff5d6b42bb647031b7f238d89267a7b3
Capra/Ibex-1 <capra@clubcapra.com>
2014-11-12 14:38:05 -0500

    git config in setup

--- setup.sh
+++ setup.sh
@@ -6,6 +6,17 @@ source devel/setup.bash
 
 cwd="$PWD"
 
+#git config stuff
+git config --global fetch.recurseSubmodules true
+git config --global alias.st status
+git config --global alias.ci commit
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global color.ui true
+git config --global color.status auto
+git config --global color.branch auto
+git config --global color.diff true
+
 #for i in `grep path .gitmodules | sed 's/.*= //'`; do
 #    cd $i
 ##    git checkout master
