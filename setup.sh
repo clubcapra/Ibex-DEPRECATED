a99b0642e134390cd3dc8162803baab003cb06d6
Capra/Ibex-1 <capra@clubcapra.com>
2014-11-12 14:41:55 -0500

    details in setup

--- setup.sh
+++ setup.sh
@@ -1,11 +1,3 @@
-git submodule init  
-git submodule update
-git submodule update --init --recursive
-catkin_make
-source devel/setup.bash
-
-cwd="$PWD"
-
 #git config stuff
 git config --global fetch.recurseSubmodules true
 git config --global alias.st status
@@ -16,6 +8,19 @@ git config --global color.ui true
 git config --global color.status auto
 git config --global color.branch auto
 git config --global color.diff true
+
+#git submodules
+git submodule init  
+git submodule update --init --recursive
+
+#build workspace
+catkin_make
+
+#source workspace
+source devel/setup.bash
+
+cwd="$PWD"
+
 
 #for i in `grep path .gitmodules | sed 's/.*= //'`; do
 #    cd $i
