c8dc78aba2c0cfa7a75fae275648ff2f8e0c9573
yohtm <yohan.trepanier@gmail.com>
2014-11-15 16:34:16 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,31 @@
+#install ROS
+echo "Installing ROS..."
+
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu trusty main" > /etc/apt/sources.list.d/ros-latest.list'
+wget https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -O - | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install ros-indigo-desktop-full ros-indigo-lms1xx ros-indigo-robot-pose-ekf ros-indigo-joy ros-indigo-nmea-* ros-indigo-move-base ros-indigo-map-server ros-indigo-hector-slam ros-indigo-gmapping ros-indigo-octomap-ros ros-indigo-octomap-rviz-plugins libgsl0-dev
+
+echo "Installing ROS done"
+
+#PATH stuff
+echo "Updating PATH..."
+
+export IBEX_HOME=~/Ibex
+source /opt/ros/indigo/setup.bash
+source $IBEX_HOME/devel/setup.bash
+
+echo "Updating PATH done"
+
+#Rosdep
+echo "Setting up rosdep..."
+sudo rosdep init
+rosdep update
+
+echo "Setting up rosdep done"
+
 #git config stuff
+echo "Configuring git..."
 git config --global fetch.recurseSubmodules true
 git config --global alias.st status
 git config --global alias.ci commit
@@ -8,13 +35,19 @@ git config --global color.ui true
 git config --global color.status auto
 git config --global color.branch auto
 git config --global color.diff true
+echo "Configuring git done"
 
 #git submodules
+echo "Setting up submodules..."
 git submodule init  
 git submodule update --init --recursive
+git submodule foreach git pull origin master
+echo "Setting up submodules done."
 
 #build workspace
+echo "Building workspace..."
 catkin_make
+echo "Building workspace done"
 
 #source workspace
 source devel/setup.bash
