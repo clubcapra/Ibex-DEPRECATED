d82543d3e3333cd4891368bb7ed468c275e41ac5
Philippe Gorley <gorley.philippe@gmail.com>
2015-04-23 17:17:36 -0400

    node qui lit un fichier json de goal pi les envoie au goal_manager

---
+++ src/capra_ai/scripts/goal_loader.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import json
+import rospy
+from geometry_msgs.msg import PoseStamped, Quaternion
+from sensor_msgs.msg import NavSatFix
+from capra_gps.srv import AddLatlongGoal
+
+
+class GoalLoader():
+
+    def __init__(self):
+        self.msgs = []
+        file_path = rospy.get_param('file')
+        with open(file_path) as data_file:
+            self.data = json.load(data_file)
+        self.parse()
+        self.send()
+
+    def parse(self):
+        for point in self.data:
+            if point['gps'] == 1:
+                pose_msg = self.gpstopose(point)
+                self.msgs.append(pose_msg)
+            else:
+                pose_msg = PoseStamped()
+                pose_msg.pose.position.x = point['x']
+                pose_msg.pose.position.y = point['y']
+                pose_msg.pose.orientation = Quaternion(w = 1)
+                self.msgs.append(pose_msg)
+
+    def send(self):
+        # publish on /goal_manager/waypoint in order of file
+        self.pose_pub('/goal_manager/waypoint', PoseStamped, queue_size = 10)
+        for pose_msg in self.msgs:
+            self.pose_pub.publish(pose_msg)
+
+    def gpstopose(self, coords):
+        # create NavSatFix msg
+        nav_msg = NavSatFix()
+        nav_msg.header.stamp = rospy.get_rostime()
+        nav_msg.header.frame_id = 'odom'
+        # x is longitude, y is latitude
+        nav_msg.longitude = coords['x']
+        nav_msg.latitude = coords['y']
+        # convert using /latlong_goal_node/AddLatLongGoal service
+        rospy.wait_for_service('/latlong_goal_node/AddLatLongGoal')
+        convert_to_latlong = rospy.ServiceProxy('/latlong_goal_node/AddLatLongGoal', AddLatlongGoal)
+        # returns PoseStamped
+        pose_msg = convert_to_latlong(nav_msg)
+        return pose_msg
+
+
+if __name__ == '__main__':
+    rospy.init_node('goal_loader')
+    try:
+        GoalLoader()
+    except rospy.ROSInterruptException: pass
