1779353189559cf4a058aaf042f6bcba15df39f7
Philippe Gorley <gorley.philippe@gmail.com>
2015-04-23 17:22:14 -0400

    sorti la creation du service proxy de la boucle

--- src/capra_ai/scripts/goal_loader.py
+++ src/capra_ai/scripts/goal_loader.py
@@ -18,10 +18,12 @@ class GoalLoader():
         self.send()
 
     def parse(self):
+        rospy.wait_for_service('/latlong_goal_node/AddLatLongGoal')
+        convert_to_latlong = rospy.ServiceProxy('/latlong_goal_node/AddLatLongGoal', AddLatlongGoal)
         for point in self.data:
             if point['gps'] == 1:
                 pose_msg = self.gpstopose(point)
-                self.msgs.append(pose_msg)
+                self.msgs.append(pose_msg, convert_to_latlong)
             else:
                 pose_msg = PoseStamped()
                 pose_msg.pose.position.x = point['x']
@@ -35,7 +37,7 @@ class GoalLoader():
         for pose_msg in self.msgs:
             self.pose_pub.publish(pose_msg)
 
-    def gpstopose(self, coords):
+    def gpstopose(self, coords, convert_to_latlong):
         # create NavSatFix msg
         nav_msg = NavSatFix()
         nav_msg.header.stamp = rospy.get_rostime()
@@ -44,8 +46,6 @@ class GoalLoader():
         nav_msg.longitude = coords['x']
         nav_msg.latitude = coords['y']
         # convert using /latlong_goal_node/AddLatLongGoal service
-        rospy.wait_for_service('/latlong_goal_node/AddLatLongGoal')
-        convert_to_latlong = rospy.ServiceProxy('/latlong_goal_node/AddLatLongGoal', AddLatlongGoal)
         # returns PoseStamped
         pose_msg = convert_to_latlong(nav_msg)
         return pose_msg
