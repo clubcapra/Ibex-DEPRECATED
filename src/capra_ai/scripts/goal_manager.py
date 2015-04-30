ef92256577f1069027821695cce244bd26403652
Philippe Gorley <gorley.philippe@gmail.com>
2015-04-30 16:02:02 -0400

    Can now loop goals

--- src/capra_ai/scripts/goal_manager.py
+++ src/capra_ai/scripts/goal_manager.py
@@ -12,8 +12,9 @@ class GoalManager():
 
     def __init__(self):
         self.goals = []
-        self.counter = 0
         self.current_idx = -1
+        self.count = 0
+        self.loop_goals = bool(rospy.get_param("~loop", default=False))
         rospy.Subscriber("~waypoint", PoseStamped, self.pose_received_callback)  # receive goals from other nodes
         rospy.Subscriber("/clicked_point", PointStamped, self.point_received_callback)  # rviz goals
         rospy.Subscriber("/move_base/status", GoalStatusArray, self.status_updated_callback)  # track status of goals
@@ -44,18 +45,18 @@ class GoalManager():
     def next_goal(self, idx = 0):
         # idx is current goal
         if len(self.goals) > idx and idx > self.current_idx:
+            # update goal id
             self.current_idx = idx
             self.goal_pub.publish(self.goals[idx])
             rospy.loginfo("Published goal # %i" % (idx + 1))
 
     def add_waypoint(self, pose):  # PoseStamped
         now = rospy.get_rostime()
-        self.counter = self.counter + 1
+        self.count = self.count + 1
         msg = MoveBaseActionGoal()
         msg.goal.target_pose = pose
         msg.goal_id.stamp = now
-        # id format is "[node_name]_[goal_count]_[time]"
-        msg.goal_id.id = "%s_%i_%i_%i" % (rospy.get_name(), self.counter, now.secs, now.nsecs)
+        msg.goal_id.id = "%s_%i_%i_%i" % (rospy.get_name(), self.count, now.secs, now.nsecs)
         self.goals.append(msg)
         position = msg.goal.target_pose.pose.position
         rospy.loginfo("Received goal:\n\tx:%f\n\ty:%f" % (position.x, position.y))
@@ -73,17 +74,34 @@ class GoalManager():
         pcl.header.frame_id = "odom"
         return pcl
 
+    def reset_goal_list(self):
+        # update goal ids
+        # set current idx to -1
+        for idx, msg in enumerate(self.goals):
+            now = rospy.get_rostime()
+            self.count = self.count + 1
+            msg.goal_id.stamp = now
+            msg.goal_id.id = "%s_%i_%i_%i" % (rospy.get_name(), self.count, now.secs, now.nsecs)
+            rospy.loginfo("Goal # %i id is now %s" % (self.count, msg.goal_id.id))
+            self.goals[idx] = msg
+        self.current_idx = -1
+
     def status_updated_callback(self, msg):  # GoalStatusArray
         intermediate_statuses = [GoalStatus.PENDING, GoalStatus.ACTIVE, GoalStatus.RECALLING, GoalStatus.PREEMPTING]
         for goal in msg.status_list:
             goal_idx = self.get_index_of(goal.goal_id.id)
             if goal.status == GoalStatus.SUCCEEDED:  # do a little dance to celebrate
                 self.wait_for_goal()
-                self.next_goal(goal_idx + 1)
+                if self.loop_goals and goal_idx + 1 == len(self.goals):
+                    rospy.loginfo("Now looping back to first goal")
+                    self.reset_goal_list()
+                    self.next_goal()
+                else:
+                    self.next_goal(goal_idx + 1)
             elif goal.status in intermediate_statuses:  # transitional state, so just wait
                 pass
             else:  # the robot's dead
-                rospy.logerr("Goal invalidated. Goal status number: %i" % goal.status)
+                rospy.logerr("Goal invalidated. %s" % goal.text)
 
     def pose_received_callback(self, msg):  # PoseStamped
         self.add_waypoint(msg)
