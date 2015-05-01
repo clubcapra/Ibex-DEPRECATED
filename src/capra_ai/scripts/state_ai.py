8676594d2eb52750ed2eea999e63916a8e62c0b8
Capra/Ibex-2 <log@clubcapra.com>
2015-05-01 15:11:09 -0400

    config for demo: many costmap tweaks, made collision box of robot smaller in gazebo

--- src/capra_ai/scripts/state_ai.py
+++ src/capra_ai/scripts/state_ai.py
@@ -3,18 +3,41 @@
 
 import rospy
 from ai_util import *
+from move_base_msgs.msg import MoveBaseActionGoal
+from std_msgs.msg import Bool
 
 class StateAi:
+
+    def goal_callback(self, msg):
+        self.goal_count += 1
+        rospy.loginfo("goal sent, count is now {}".format(self.goal_count))
+
+        if self.pause:
+            rospy.sleep(2)
+            set_robot_speed(1.8)
+            self.pause = False
+
+        if self.goal_count == 3:
+
+            set_robot_speed(0.0)
+            self.pub_generate(Bool(True))
+            start_node("capra_2dnav", "obstacle_generator.py")
+            self.pause = True
+
 
     def __init__(self):
         rospy.init_node("state_ai")
 
-        self.test_node()
+        rospy.Subscriber("/move_base/goal", MoveBaseActionGoal, self.goal_callback)
+        self.pub_generate = rospy.Publisher("/obstacle_generator/generate_circle", Bool, queue_size=10)
+
+        self.goal_count = 0
+        self.pause = False
 
         rospy.spin()
 
-    def test_node(self):
-        set_robot_speed(2.0)
+        #def test_node(self):
+        #    set_robot_speed(2.0)
 
 if __name__ == "__main__":
     try:
