99b58325490a5e340087ed8b5fdefae524e030a9
Ibex-1 <robot@clubcapra.com>
2015-05-08 16:25:07 -0400

    new params in inflate_pointcloud

--- src/capra_2dnav/scripts/inflate_pointcloud.py
+++ src/capra_2dnav/scripts/inflate_pointcloud.py
@@ -3,19 +3,23 @@
 import rospy
 from sensor_msgs.msg import PointCloud2
 import sensor_msgs.point_cloud2 as pc2
+import math
+import operator
 
 rospy.init_node('inflate_pointcloud')
 
-res = 0.2
 pub = rospy.Publisher(rospy.get_param('out_topic', 'cloud_in'), PointCloud2, queue_size=10)
-
+n = rospy.get_param('count', 5)
+res = 0.2
+angle = math.pi * 2 / n
+deltas = [[math.cos(angle * i) * res, math.sin(angle * i) * res, 0] for i in range(0, n)]
 
 def handle_pointcloud(msg):
     results = []
     points = pc2.read_points(msg)
-
+    
     for x, y, z in points:
-        results.extend([[x + res * i, y + res * j, z] for i in [-1, 0, 1] for j in [-1, 0, 1]])
+        results.extend([[x + a, y + b, z + c] for a, b, c in deltas])
 
     pub.publish(pc2.create_cloud_xyz32(msg.header, results))
 
