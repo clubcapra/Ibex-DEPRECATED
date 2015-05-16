31cb6828d5a27b26f53a8819f4d2eac3f6c070bd
Mathieu Gascon-Lefebvre <mathieuglefebvre@gmail.com>
2015-05-16 13:19:33 -0400

    Started capra_camera refactoring.

--- src/capra_camera/scripts/camera_node.py
+++ src/capra_camera/scripts/camera_node.py
@@ -7,38 +7,66 @@ import rospy
 from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
 import cv2
+from includes.OutputGrabber import OutputGrabber
 
 
-rospy.init_node('capra_camera')
+class CameraNode:
+    def __init__(self):
+        # Loading library.
+        rospy.init_node('capra_camera')
+        if not self._load_camera():
+            rospy.logerr("Cannot open PvCamera. Missing camera library.")
+            return
 
-try:
-    USE_PV_CAM = True
-    import camera
-except:
-    USE_PV_CAM = False
-    rospy.logerr("Cannot open PvCamera. Missing camera library.")
-    
-if USE_PV_CAM:
+        # CvBridge to convert the opencv image to a ros image.
+        self.bridge = CvBridge()
 
-    cam = camera.Camera()
-    cam.initialize()
-    cam.start()
-    images = cam.getCam()
-    
-    bridge = CvBridge()
-    image_pub = rospy.Publisher("~image_uncompressed", Image, queue_size=10)
-    small_pub = rospy.Publisher("~small_uncompressed", Image, queue_size=10)
-    r = rospy.Rate(20)
-    
-    while not rospy.is_shutdown():
-        img = images[cam.getFrame()]
-        small = cv2.resize(img, (0,0), fx=0.5, fy=0.5)
-        # utiliser l'image full size cause parfois des bars dans l'image. c'est peut-être causé par le fait que le 
-        # publish envoie l'image et qu'un process asynchrone de ROS utilise termine l'envoi de l'image APRÈS qu'on 
-        # ait callé un getFrame de nouveau. Le getFrame bloque l'image jusqu'au prochain call getFrame, mais dans le 
-        # cas décrit, l'image serait débloqué avant que ROS ait terminé de la publier. Il faudrait donc en faire une 
-        # copie et publier cette copie pour être sur qu'elle ne se fera pas overwriter
-        
-        #image_pub.publish(bridge.cv2_to_imgmsg(img, "bgr8"))
-        small_pub.publish(bridge.cv2_to_imgmsg(small, "bgr8"))
-        r.sleep()    
+        # Initialize camera.
+        rospy.loginfo("Initializing camera")
+        initialized = False
+        error_printed = False
+        while not initialized and not rospy.is_shutdown():
+            initialized = self._initialize_camera()
+            if not initialized and not error_printed:
+                error_printed = True
+                rospy.logerr("Could not initialize camera. Retrying...")
+        rospy.loginfo("Camera initialized.")
+
+        # Ros topic and topic rate.
+        self.image_pub = rospy.Publisher("~image_uncompressed", Image, queue_size=10)
+        rate = rospy.Rate(20)
+
+        while not rospy.is_shutdown():
+            self.image_pub.publish(self._get_last_image())
+            rate.sleep()
+
+    def _get_last_image(self):
+        image = self.images[self.camera.getFrame()]
+        # Utiliser l'image full size cause parfois des barres dans l'image. c'est peut-être causé par le fait que le
+        # publish envoie l'image et qu'un process asynchrone de ROS utilise termine l'envoi de l'image APRÈS qu'on
+        # ait callé un getFrame de nouveau. Le getFrame bloque l'image jusqu'au prochain call getFrame, mais dans le
+        # cas décrit, l'image serait débloquée avant que ROS ait terminé de la publier. Il faudrait donc en faire une
+        # copie et publier cette copie pour être sur qu'elle ne se fera pas overwritée.
+        small = cv2.resize(image, (0,0), fx=0.5, fy=0.5)
+        return self.bridge.cv2_to_imgmsg(small, "bgr8")
+
+    def _load_camera(self):
+        try:
+            import camera
+            with OutputGrabber():
+                self.camera = camera.Camera()
+            return True
+        except:
+            return False
+
+    def _initialize_camera(self):
+        try:
+            with OutputGrabber():
+                self.camera.initialize()
+            self.camera.start()
+            self.images = self.camera.getCam()
+            return True
+        except RuntimeError:
+            return False
+
+node = CameraNode()
