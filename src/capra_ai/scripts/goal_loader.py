d0edf0fa8587a5c6c52c11ffcd408472cbb9f4b6
Philippe Gorley <gorley.philippe@gmail.com>
2015-05-18 19:52:57 -0400

    fixed clear goal service, dynamic precision distance, added priority to goal messages

--- src/capra_ai/scripts/goal_loader.py
+++ src/capra_ai/scripts/goal_loader.py
@@ -6,6 +6,7 @@ import os.path
 from geometry_msgs.msg import PoseStamped, Quaternion
 from sensor_msgs.msg import NavSatFix
 from capra_gps.srv import AddLatlongGoal
+from capra_ai.msg import GoalWithPriority
 
 
 class GoalLoader():
@@ -15,7 +16,10 @@ class GoalLoader():
         rospy.sleep(2.0)  # wait for move_base to be ready
         self.msgs = []
         self.pub_topic = '/goal_manager/waypoint'
-        self.pose_pub = rospy.Publisher(self.pub_topic, PoseStamped, queue_size=10)
+        self.goal_pub = rospy.Publisher(self.pub_topic, GoalWithPriority, queue_size=10)
+        rate = rospy.Rate(10)
+        while self.goal_pub.get_num_connections() == 0:
+            rate.sleep()
         file_path = rospy.get_param("~file")
         rospy.loginfo("Fetching waypoint data from: %s" % file_path)
 
@@ -45,20 +49,24 @@ class GoalLoader():
             rospy.loginfo("Point is %s" % str(point))
             if point['gps'] == 1:
                 pose_msg = self.convert_gps_to_pose(point)
-                self.msgs.append(pose_msg)
             else:
                 pose_msg = PoseStamped()
-                pose_msg.header.frame_id = 'odom'
                 pose_msg.pose.position.x = point['x']
                 pose_msg.pose.position.y = point['y']
                 pose_msg.pose.orientation = Quaternion(w = 1)
-                self.msgs.append(pose_msg)
+            # goal_id will be filled in later on in the goal_manager
+            # leave it blank
+            goal_msg = GoalWithPriority()
+            goal_msg.pose = pose_msg.pose
+            goal_msg.priority = point['priorite']
+            self.msgs.append(goal_msg)
 
     def send(self):
         # publish on /goal_manager/waypoint in order of file
         rospy.loginfo("Publishing %i points on %s" % (len(self.msgs), self.pub_topic))
-        for pose_msg in self.msgs:
-            self.pose_pub.publish(pose_msg)
+        for goal_msg in self.msgs:
+            self.goal_pub.publish(goal_msg)
+            rospy.loginfo("Published goal on /goal_manager/waypoint")
 
     def convert_gps_to_pose(self, coords):
         # create NavSatFix msg
