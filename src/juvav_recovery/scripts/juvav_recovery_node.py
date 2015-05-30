d4e8a5aa09c74d61775ff69814c61bf5db8245bd
Capra/Ibex-2 <log@clubcapra.com>
2015-05-29 21:08:20 -0400

    succesfully added undo cmd_vel behavior. As a test, in gazebo robot will undo cmd_vel 8 seconds after he started his run.

--- src/juvav_recovery/scripts/juvav_recovery_node.py
+++ src/juvav_recovery/scripts/juvav_recovery_node.py
@@ -5,16 +5,25 @@ import rospy
 from sensor_msgs.msg import Image, PointCloud2
 import sensor_msgs.point_cloud2 as pc2
 from geometry_msgs.msg import Twist
+from nav_msgs.msg import Path
+
 import tf
 from cv_bridge import CvBridge
 from math import *
 from juvav_recovery.srv import *
 
+from numpy import array
+from numpy.linalg import inv
+
+from collections import deque
+
+undo_cmd_vel = False
+
 
 class JuvavRecoveryNode:
 
     def __init__(self):
-
+        self.undo_stack = deque((), 400)
         self.requested_vel = None
         self.requested_vel_time = 0
 
@@ -23,11 +32,18 @@ class JuvavRecoveryNode:
 
         self.smart_cmd_vel_publisher = rospy.Publisher('/smart_cmd_vel', Twist, queue_size=10)
         rospy.Subscriber('/cmd_vel', Twist, self.cmd_vel_callback)
+        rospy.Subscriber('/move_base/TrajectoryPlannerROS/local_plan', Path, self.local_path_callback)
 
         listener = tf.TransformListener()
 
+        count = 0
+
         rate = rospy.Rate(4)  # 4Hz
         while not rospy.is_shutdown():
+
+            count += 1
+            if count == 50:
+                self.handle_run(None)
 
             tf_found = False
 
@@ -35,7 +51,7 @@ class JuvavRecoveryNode:
                 (trans, rotQ) = listener.lookupTransform('/base_footprint', '/odom', rospy.Time(0))
                 rotE = tf.transformations.euler_from_quaternion(rotQ)
 
-                print trans, rotQ
+                #print trans, rotQ
 
             except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
                 # output error only if the tf was previously found and is now lost.
@@ -46,17 +62,60 @@ class JuvavRecoveryNode:
 
     @staticmethod
     def handle_run(msg):
-        print "Got a call on RUN service!"
+        global undo_cmd_vel
+        undo_cmd_vel = True
+
+        print 'inb4'
+
         return True
 
     def publish_smart_cmd_vel(self, event):
-        self.smart_cmd_vel_publisher.publish(self.requested_vel)
+        global undo_cmd_vel
+
+        if undo_cmd_vel:
+            try:
+                twist = self.undo_stack.pop()
+                #assert isinstance(twist, Twist)
+
+                twist.linear.x *= -1.0
+                twist.linear.y *= -1.0
+                twist.linear.z *= -1.0
+
+                twist.angular.x *= -1.0
+                twist.angular.y *= -1.0
+                twist.angular.z *= -1.0
+
+                self.smart_cmd_vel_publisher.publish(twist)
+            except IndexError:
+                undo_cmd_vel = False
+        else:
+            self.undo_stack.append(self.requested_vel)
+            self.smart_cmd_vel_publisher.publish(self.requested_vel)
+            print '(', self.requested_vel.linear.x, self.requested_vel.linear.y, self.requested_vel.linear.z, ') (', \
+                self.requested_vel.angular.x, self.requested_vel.angular.y, self.requested_vel.angular.z, ')'
+
 
     def cmd_vel_callback(self, msg):
         self.requested_vel = msg
         self.requested_vel_time = rospy.get_time()
         self.publish_smart_cmd_vel(None)
 
+    def local_path_callback(self, msg):
+        """
+        1( 0.0 0.0 0.0 ) ( 0.0 0.0 0.0 )
+
+        ( 0.0 0.0 0.0 ) ( 0.0 0.0 0.0 )
+        1
+        ( 0.0 0.0 0.0 ) ( 0.0 0.0 0.0 )
+        1
+        1
+
+        :param msg:
+        :return:
+        """
+
+        assert isinstance(msg, Path)
+        print len(msg.poses)
 
 if __name__ == "__main__":
     try:
--- src/juvav_recovery/src/juvav_recovery.cpp
+++ src/juvav_recovery/src/juvav_recovery.cpp
@@ -47,27 +47,7 @@ JuvavRecovery::JuvavRecovery(): global_costmap_(NULL), local_costmap_(NULL),
 void JuvavRecovery::initialize(std::string name, tf::TransformListener* tf,
     costmap_2d::Costmap2DROS* global_costmap, costmap_2d::Costmap2DROS* local_costmap){
   if(!initialized_){
-    name_ = name;
-    tf_ = tf;
-    global_costmap_ = global_costmap;
-    local_costmap_ = local_costmap;
 
-    //get some parameters from the parameter server
-    ros::NodeHandle private_nh("~/" + name_);
-    ros::NodeHandle blp_nh("~/TrajectoryPlannerROS");
-
-    //we'll simulate every degree by default
-    private_nh.param("sim_granularity", sim_granularity_, 0.017);
-    private_nh.param("frequency", frequency_, 20.0);
-
-    blp_nh.param("acc_lim_th", acc_lim_th_, 3.2);
-    blp_nh.param("max_rotational_vel", max_rotational_vel_, 1.0);
-    blp_nh.param("min_in_place_rotational_vel", min_rotational_vel_, 0.4);
-    blp_nh.param("yaw_goal_tolerance", tolerance_, 0.10);
-
-    world_model_ = new base_local_planner::CostmapModel(*local_costmap_->getCostmap());
-
-    initialized_ = true;
   }
   else{
     ROS_ERROR("You should not call initialize twice on this object, doing nothing");
@@ -92,64 +72,12 @@ void JuvavRecovery::runBehavior(){
 
   ros::Rate r(frequency_);
   ros::NodeHandle n;
-  ros::Publisher vel_pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 10);
 
-  tf::Stamped<tf::Pose> global_pose;
-  local_costmap_->getRobotPose(global_pose);
-
-  double current_angle = -1.0 * M_PI;
-
-  bool got_180 = false;
-
-  double start_offset = 0 - angles::normalize_angle(tf::getYaw(global_pose.getRotation()));
   while(n.ok()){
-    local_costmap_->getRobotPose(global_pose);
 
-    double norm_angle = angles::normalize_angle(tf::getYaw(global_pose.getRotation()));
-    current_angle = angles::normalize_angle(norm_angle + start_offset);
+    ros::ServiceClientOptions options = ros::ServiceClientOptions("/juvav_recovery_node/run", "ea7d52e23d23b5302df35cf5643628da", false, std::map<std::string, std::string>());
 
-    //compute the distance left to rotate
-    double dist_left = M_PI - current_angle;
-
-    double x = global_pose.getOrigin().x(), y = global_pose.getOrigin().y();
-
-    //check if that velocity is legal by forward simulating
-    double sim_angle = 0.0;
-    while(sim_angle < dist_left){
-      double theta = tf::getYaw(global_pose.getRotation()) + sim_angle;
-
-      //make sure that the point is legal, if it isn't... we'll abort
-      double footprint_cost = world_model_->footprintCost(x, y, theta, local_costmap_->getRobotFootprint(), 0.0, 0.0);
-      if(footprint_cost < 0.0){
-        ROS_ERROR("Juvav Recovery can't rotate in place because there is a potential collision. Cost: %.2f", footprint_cost);
-        return;
-      }
-
-      sim_angle += sim_granularity_;
-    }
-
-    //compute the velocity that will let us stop by the time we reach the goal
-    double vel = sqrt(2 * acc_lim_th_ * dist_left);
-
-    //make sure that this velocity falls within the specified limits
-    vel = std::min(std::max(vel, min_rotational_vel_), max_rotational_vel_);
-
-    geometry_msgs::Twist cmd_vel;
-    cmd_vel.linear.x = 0.0;
-    cmd_vel.linear.y = 0.0;
-    cmd_vel.angular.z = vel;
-
-    vel_pub.publish(cmd_vel);
-
-    //makes sure that we won't decide we're done right after we start
-    if(current_angle < 0.0)
-      got_180 = true;
-
-    //if we're done with our in-place rotation... then return
-    if(got_180 && current_angle >= (0.0 - tolerance_))
-      return;
-
-    r.sleep();
+    ros::ServiceClient client = n.serviceClient(options);
   }
 }
 };
