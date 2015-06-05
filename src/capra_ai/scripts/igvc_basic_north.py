1459774dbb6a27d5d94de07567286428b97cdfc6
Ibex-1 <robot@clubcapra.com>
2015-06-05 17:24:45 -0400

    Setting up basic course. Do not use yet

--- src/capra_ai/scripts/igvc_basic_north.py
+++ src/capra_ai/scripts/igvc_basic_north.py
@@ -14,9 +14,28 @@ class IGVCBasicNorth(StateAi):
         self.generate_circle(7.0, pi/4, 2 * pi - pi/4, pi/270.0, 20)
 
     def on_goal_changed(self, goal_msg):
-        rospy.loginfo(goal_msg.priority)
+        rospy.loginfo("Targeting goal with priority: {}", goal_msg.priority)
+        if goal_msg.priority == 306:
+            self.clear_octomap(self.start_pos, 10, 10)
+
+        if goal_msg.priority == 103: ## First GOAL
+            pass
+
+        if goal_msg.priority == 304: ## MiddlePoint
+            pass
+
+        if goal_msg.priority == 105: ## Last GOAL
+            pass
+
+        if goal_msg.priority == 302:
+            self.generate_bar(8, 2, -1)
+
+
+
+
 
     def on_last_goal_reached(self, msg):
+        rospy.loginfo("WE FINISHED THE BASIC COURSE !!! CONGRATS EVERYONE !!!")
         pass
 
 if __name__ == "__main__":
--- src/capra_ai/scripts/igvc_basic_south.py
+++ src/capra_ai/scripts/igvc_basic_south.py
@@ -14,7 +14,21 @@ class IGVCBasicSouth(StateAi):
         self.generate_circle(7.0, pi/4, 2 * pi - pi/4, pi/270.0, -1)
 
     def on_goal_changed(self, goal_msg):
-        rospy.loginfo(goal_msg.priority)
+        rospy.loginfo("Targeting goal with priority: {}", goal_msg.priority)
+        if goal_msg.priority == 302:
+            self.clear_octomap(self.start_pos, 10, 10)
+
+        if goal_msg.priority == 103: ## First GOAL
+            pass
+
+        if goal_msg.priority == 304: ## MiddlePoint
+            pass
+
+        if goal_msg.priority == 105: ## Last GOAL
+            pass
+
+        if goal_msg.priority == 306:
+            self.generate_bar(8, 2, -1)
 
     def on_last_goal_reached(self, msg):
         pass
