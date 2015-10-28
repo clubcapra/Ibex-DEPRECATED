c9f93c864ca3d47fdedea635a24bc7b37777e2fb
Capra/Ibex-2 <log@clubcapra.com>
2015-10-27 21:56:29 -0400

    Motors

--- src/roboteq_motor/scripts/roboteq_motor_node.py
+++ src/roboteq_motor/scripts/roboteq_motor_node.py
@@ -1,113 +1,47 @@
-__author__ = 'capra'
-
 #!/usr/bin/env python
 
 import roslib
-roslib.load_manifest('animatics_smart_motor')
+roslib.load_manifest('roboteq_motor')
 import rospy
-from includes.motor_controller import MotorController
-from animatics_smart_motor.msg import MotorsConnected
-from includes.config import Config
-from geometry_msgs.msg import Twist
-from nav_msgs.msg import Odometry
+from roboteq_msgs.msg import Feedback, Command, Status
+
 import math
 
+class RoboteqMotor:
+    def __init__(self):
+        rospy.init_node("roboteq_motor")
+        rospy.Subscriber("/roboteq_driver/feedback", Feedback, self.feedback_report)
+        rospy.Subscriber("/roboteq_driver/status", Status, self.status_report)
+        self.commander = rospy.Publisher("/roboteq_driver/cmd", Command, queue_size=10)
 
-requested_vel = None
-requested_vel_time = 0
-last_vel = Twist()
-motor_controller = None
-connected_msg = MotorsConnected()
-disconnected_msg = MotorsConnected()
-connected_msg.connected = True
-disconnected_msg.connected = False
-
-def publish_odom(event):
-    global motor_controller, od
-    pos = motor_controller.position
-    covariance = Config.get_covariance_matrix()
-
-    odom = Odometry()
-    odom.header.frame_id = "odom"
-    odom.child_frame_id = "base_link"
-
-    if rospy.get_time() - pos.position_time > 50.0/1000:
-        odom.header.stamp = rospy.Time.now()
-    else:
-        odom.header.stamp = rospy.Time.from_sec(pos.position_time)
-
-    odom.pose.pose.position.x = pos.x
-    odom.pose.pose.position.y = pos.y
-
-    odom.pose.pose.orientation.z = math.sin(pos.yaw / 2)
-    odom.pose.pose.orientation.w = math.cos(pos.yaw / 2)
-    odom.pose.covariance = covariance
-
-    odom.twist.twist.linear.x = pos.linear_vel
-    odom.twist.twist.angular.z = pos.angular_vel
-    odom.twist.covariance = covariance
-
-    odom_publisher.publish(odom)
-
-    #print position.x, position.y
+        while True:
+            self.run()
+            rospy.sleep(10)
 
 
-def cmd_vel_callback(msg):
-    global requested_vel, requested_vel_time
-    requested_vel = msg
-    requested_vel_time = rospy.get_time()
+    def run(self):
+        motorCommand = Command()
+        motorCommand.setpoint = 100.0
+        motorCommand.mode = Command.MODE_VELOCITY
+        self.commander.publish(motorCommand)
 
 
-def velocity_timer(event):
-    global requested_vel, requested_vel_time, last_vel, motor_controller
-    if rospy.get_time() - requested_vel_time > Config.get_watchdog_timeout():
-        if last_vel.linear.x != 0 or last_vel.angular.z != 0:
-            last_vel = Twist()
-            motor_controller.set_velocity(0, 0)
-    else:
-        if last_vel.linear.x != requested_vel.linear.x or last_vel.angular.z != requested_vel.angular.z:
-            last_vel = requested_vel
-            motor_controller.set_velocity(last_vel.linear.x, last_vel.angular.z)
+
+    def status_report(self, status):
+
+        rospy.loginfo("Reporting status, fault {} status {}".format(status.fault, status.status))
+
+    def feedback_report(self, feedback):
+
+        rospy.loginfo("Reporting feedback {}".format(feedback.motor_current))
+
 
 
 if __name__ == "__main__":
-    rospy.init_node('animatics_smart_motor')
-    motor_controller = MotorController()
-
-    rospy.Subscriber("/cmd_vel", Twist, cmd_vel_callback)
-    odom_publisher = rospy.Publisher("/odom", Odometry, queue_size=10)
-    connected_publisher = rospy.Publisher("~connected", MotorsConnected, queue_size=5, latch=True)
-    rospy.Timer(rospy.Duration.from_sec(50.0/1000), velocity_timer)
-    rospy.Timer(rospy.Duration.from_sec(1.0/Config.get_publish_rate()), publish_odom)
-
-    error_count = 0
-    max_timeout_count = Config.get_max_timeout_count()
-    connected = False
-
-    rospy.loginfo("Connecting to the motors...")
-    while not rospy.is_shutdown():
-        if connected:
-            s = rospy.get_time()
-            if motor_controller.update():
-                error_count = 0
-            else:
-                error_count += 1
-
-            if error_count > max_timeout_count:
-                connected_publisher.publish(disconnected_msg)
-                rospy.logerr("Lost connection to the motors. Reconnecting...")
-                motor_controller.disconnect()
-                connected = False
-                rospy.sleep(0.1)
-        else:
-            connected = motor_controller.initialize()
-            if not connected:
-                motor_controller.disconnect()
-                rospy.sleep(0.5)
-            else:
-                rospy.loginfo("Connected to the motors.")
-                connected_publisher.publish(connected_msg)
-                motor_controller.set_velocity(last_vel.linear.x, last_vel.angular.z)
+    try:
+        motor = RoboteqMotor()
+    except rospy.ROSInterruptException:
+        rospy.logerr('RoboteqMotor: Core interruption')
 
 
 
--- src/seagoatvision_ros/scripts/CapraVision/server/filters/implementation/__init__.py
+++ src/seagoatvision_ros/scripts/CapraVision/server/filters/implementation/__init__.py
@@ -3,7 +3,7 @@
 #    Copyright (C) 2012  Club Capra - capra.etsmtl.ca
 #
 #    This filename is part of CapraVision.
-#    
+#
 #    CapraVision is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
@@ -19,12 +19,12 @@
 """ Filters implementations
     Rules:
         - Filter must be a class
-        - Filter must have a execute(image) function that 
+        - Filter must have a execute(image) function that
             returns the processed image
-        - Filter can have a configure() function to configure the 
+        - Filter can have a configure() function to configure the
             object after creation
         - If there are data members that must not be saved, the member name
-            must start with an underscore.  
+            must start with an underscore.
             Eg: self._dont_save = 123 # value will not be save
                 self.save = 456 # value will be saved"""
 
@@ -45,7 +45,7 @@ for f in os.listdir(os.path.dirname(__file__)):
     if not f.endswith(".py"):
         continue
     filename, _ = os.path.splitext(f)
-    code = 'from %(module)s import *' % {'module' : filename} 
+    code = 'from %(module)s import *' % {'module' : filename}
     exec code
 
 
@@ -56,9 +56,9 @@ for f in os.listdir(os.path.dirname(__file__)):
 def compile_cpp_filters():
     """
     This method finds and compile every c++ filters
-    If a c++ file changed, the file must be recompiled in a new .so file 
+    If a c++ file changed, the file must be recompiled in a new .so file
     """
-    
+
     BUILD_DIR = 'build'
     RELOAD_DIR = os.path.join('build', 'reload')
     if not os.path.exists(BUILD_DIR):
@@ -71,7 +71,7 @@ def compile_cpp_filters():
 
     image = np.zeros((1,1), dtype=np.uint8)
     params = {}
-    
+
     def ext_code():
         """
         Return the code that calls a c++ filter
@@ -84,7 +84,7 @@ def compile_cpp_filters():
             }
 
             """
-    
+
     def init_code():
         """
         This method returns the code to initialize the parameters
@@ -94,7 +94,7 @@ def compile_cpp_filters():
         py_init_param = pip;
         init();
         """
-        
+
     def params_code():
         """
         Support code to declare parameters in the C++ filters
@@ -110,8 +110,8 @@ def compile_cpp_filters():
                     args[1] = min;
                     args[2] = max;
                     args[3] = def_val;
-                    py_init_param.call(args);            
-            } 
+                    py_init_param.call(args);
+            }
             long ParameterAsInt(const char* name, int min, int max, int def_val) {
                 if(!params.has_key(name)) {
                     init_param(name, min, max, def_val);
@@ -125,9 +125,9 @@ def compile_cpp_filters():
                     init_param(name, min, max, def_val);
                 }
                 return PyInt_AsLong(params.get(name).mcall("get_current_value"));
-            }            
+            }
         """
-        
+
     def help_code():
         """
         Return the code that returns the help string from a c++ file
@@ -139,7 +139,7 @@ def compile_cpp_filters():
             return_val = "";
             #endif
             """
-    
+
     def config_code():
         """
         Code to reconfigure the filter
@@ -147,24 +147,25 @@ def compile_cpp_filters():
         return """
             configure();
         """
-    
+
     def create_execute(cppfunc):
         """
         Create and return an "execute" method for the dynamically
         created class that wraps the c++ filters
         """
         def execute(self, image):
+            print image.__class__
             cppfunc(image)
             return image
         return execute
-        
+
     def create_configure(cppfunc):
         """
         """
         def configure(self):
             cppfunc()
         return configure
-    
+
     def create_init(cppfunc, params):
         """
         """
@@ -172,72 +173,72 @@ def compile_cpp_filters():
             self.params = {}
             cppfunc(self.params, self.py_init_param)
         return __init__
-    
+
     def py_init_param(self, name, min, max, def_val):
         param = Parameter(name, min, max, def_val)
         self.params[name] = param
         setattr(self, name, param)
-        
+
     dirname = os.path.dirname(__file__)
     for f in os.listdir(dirname):
         if not f.endswith(".cpp"):
             continue
         filename, _ = os.path.splitext(f)
         cppcode = open(os.path.join(dirname, f)).read()
-        
+
         #Verify if there are changes in the c++ code file.  If there are
         #changes, add a timestamp to the filter .so file name to force a
         #reimportation of the new filter.
         if g.cppfiles.has_key(filename):
             if cppcode != g.cppfiles[filename]:
                 g.cpptimestamps[filename] = str(int(time.time()))
-        
+
         g.cppfiles[filename] = cppcode
-        
+
         if g.cpptimestamps.has_key(filename):
             modname = filename + g.cpptimestamps[filename]
         else:
             modname = filename
-        
+
         #Compile filters
         #The included files are found in the .cpp file
         mod = ext_tools.ext_module(modname)
-        [mod.customize.add_header(line.replace('#include ', '')) 
-                                 for line in cppcode.split('\n') 
+        [mod.customize.add_header(line.replace('#include ', ''))
+                                 for line in cppcode.split('\n')
                                  if line.startswith('#include')]
         mod.customize.add_header("<Python.h>")
         mod.customize.add_extra_link_arg("`pkg-config --cflags --libs opencv python`")
         mod.customize.add_extra_link_arg("-L/usr/local/cuda/lib64")
-    
+
         func = ext_tools.ext_function('exec_' + filename, ext_code(),['image'])
         func.customize.add_support_code(params_code())
         func.customize.add_support_code(cppcode)
         mod.add_function(func)
-    
+
         helpfunc = ext_tools.ext_function('help_' + filename, help_code(), [])
         mod.add_function(helpfunc)
-        
+
         p = params
         pip = py_init_param
         initfunc = ext_tools.ext_function('init_' + filename, init_code(), ['p', 'pip'])
         initfunc.customize.add_support_code(params_code())
         #initfunc.customize.add_support_code(cppcode)
         mod.add_function(initfunc)
-        
+
         configfunc = ext_tools.ext_function('config_' + filename, config_code(), [])
         configfunc.customize.add_support_code(params_code())
         configfunc.customize.add_support_code(cppcode)
         mod.add_function(configfunc)
-        
+
         try:
             if g.cpptimestamps.has_key(filename):
             #Reloaded modules are saved in the reload folder for easy cleanup
                 mod.compile(RELOAD_DIR)
             else:
                 mod.compile(BUILD_DIR)
-    
+
             cppmodule = __import__(modname)
-            
+
             params = {}
             clazz = type(filename, (object,),
                  {'__init__' : create_init(
@@ -246,9 +247,9 @@ def compile_cpp_filters():
                                     getattr(cppmodule, 'config_' + filename)),
                   'execute' : create_execute(
                                     getattr(cppmodule, 'exec_' + filename)),
-                  'py_init_param' : py_init_param, 
+                  'py_init_param' : py_init_param,
                   '__doc__' : getattr(cppmodule, 'help_' + filename)()})
-            
+
             setattr(sys.modules[__name__], filename, clazz)
             del clazz
         except Exception as e:
