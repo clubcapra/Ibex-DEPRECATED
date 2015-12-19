edac307a00993316d260ee75c33a2162839f9b69
Maxime St-Pierre <maxime.st-pierre.3@ens.etsmtl.ca>
2015-12-19 09:49:54 -0400

    Added controller status to motor node

--- src/roboteq_motor/scripts/motor.py
+++ src/roboteq_motor/scripts/motor.py
@@ -3,12 +3,13 @@ class Motor:
         self.publisher = publisher
         self.last_feedback
         self.last_status
+        self.wheel_size = 0.25
 
     def set_velocity(self, velocity):
         pass
 
     def get_current_velocity(self):
-        return self.last_feedback.measured_velocity
+        return self.last_feedback.measured_velocity * 0.25
 
     def get_motor_current(self):
         return self.last_feedback.motor_current
@@ -27,9 +28,36 @@ class Motor:
 
 
     def encoder_radial_velocity(self):
-        pass
+        self.last_feedback.measured_velocity
 
     def set_mode(self, mode):
         pass
     def _status_callback(self,msg):
         self.last_status = msg
+
+    def get_controller_status(self):
+        fault = self.last_status.fault
+
+        if(fault == 1):
+
+            return "Overheat"
+
+        elif(fault == 2):
+            return "Overvoltage"
+
+        elif(fault == 4):
+            return "Undervoltage"
+
+        elif(fault == 8):
+            return "Short Detected"
+
+        elif(fault == 16):
+            return "Estop"
+
+        elif(fault == 32):
+            return "SPEXExcitation"
+        elif(fault == 64):
+            return "MOSFET Failure"
+        elif(fault == 128):
+            return "Startup Config Problem"
+
--- src/roboteq_motor/scripts/roboteq_motor_node.py
+++ src/roboteq_motor/scripts/roboteq_motor_node.py
@@ -19,8 +19,6 @@ class RoboteqMotorController:
 
         rospy.Subscriber("/cmd_vel", Twist, self.cmd_vel_callback)
 
-
-
         self.front_left_motor = Motor(rospy.Publisher("roboteq_driver/front/left", Command, queue_size=10))
         self.front_right_motor = Motor(rospy.Publisher("roboteq_driver/front/right", Command, queue_size=10))
         self.rear_left_motor = Motor(rospy.Publisher("roboteq_driver/rear/left", Command, queue_size=10))
@@ -28,9 +26,14 @@ class RoboteqMotorController:
         self.swivel = None
 
         rospy.Subscriber("roboteq_driver/front/left", Feedback, self.front_left_motor._feedback_callback())
-        rospy.Subscriber("roboteq_driver/front/right",Feedback, self.front_right_motor._feedback_callback())
+        rospy.Subscriber("roboteq_driver/front/right" ,Feedback, self.front_right_motor._feedback_callback())
         rospy.Subscriber("roboteq_driver/rear/left", Feedback, self.rear_left_motor._feedback_callback())
         rospy.Subscriber("roboteq_driver/rear/right", Feedback, self.rear_right_motor._feedback_callback())
+
+        rospy.Subscriber("roboteq_driver/front/left", Status, self.front_left_motor._status_callback())
+        rospy.Subscriber("roboteq_driver/front/right", Status, self.front_right_motor._status_callback())
+        rospy.Subscriber("roboteq_driver/rear/left", Status, self.rear_left_motor._status_callback())
+        rospy.Subscriber("roboteq_driver/rear/right", Status, self.rear_right_motor._status_callback())
 
     def velocity_timer(self, event):
         self.front_left_motor.set_velocity(4)
@@ -53,11 +56,20 @@ class RoboteqMotorController:
         self.rear_left.publish(motorCommand)
         self.rear_right.publish(motorCommand)
 
+    def get_motor_status(self):
+        front_left_motor_status = self.front_left_motor.get_controller_status()
+        front_right_motor_status = self.front_right_motor.get_controller_status()
+        rear_left_motor_status = self.rear_left_motor.get_controller_status()
+        rear_right_motor_status = self.rear_right_motor.get_controller_status()
+
+        rospy.loginfo(front_left_motor_status)
+        rospy.loginfo(front_right_motor_status)
+        rospy.loginfo(rear_left_motor_status)
+        rospy.loginfo(rear_right_motor_status)
+
 if __name__ == "__main__":
     try:
         motor = RoboteqMotorController()
     except rospy.ROSInterruptException:
         pass
-
-
 
