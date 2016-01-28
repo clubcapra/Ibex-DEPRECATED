69702eb6962a400725af913fad80a57fa7c2bc8c
maximest-pierre <maxime.st-pierre.3@ens.etsmtl.ca>
2016-01-27 19:27:41 -0500

    Added port definition to swivel

--- src/roboteq_motor/scripts/swivel.py
+++ src/roboteq_motor/scripts/swivel.py
@@ -1,7 +1,17 @@
+import serial
 
 class Swivel:
     def __init__(self):
-        self.serial_com = None
+        self.serial_com = '/dev/ttyACM0'
+        self.baud_rate = 9600
 
+        self.ser = serial.Serial(self.serial_com,self.baud_rate)
     def set_angle(self,angle):
         pass
+
+    def get_angle(self):
+
+        #add exception to close port
+        #Valeur raw
+        return float(self.ser.readline())
+
