20a07e82b3276ff34aa44d298e2995d3a1f3320b
becojo <benoit.cote.jodoin@gmail.com>
2016-06-03 22:17:20 -0400

    clear octomap recovery behavior

---
+++ src/capra_2dnav/src/clear_octomap.cpp
@@ -0,0 +1,36 @@
+#include <nav_core/recovery_behavior.h>
+#include <costmap_2d/costmap_2d_ros.h>
+#include <tf/transform_listener.h>
+#include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+#include <geometry_msgs/Point.h>
+#include <angles/angles.h>
+#include <pluginlib/class_list_macros.h>
+#include <std_srvs/Empty.h>
+
+
+
+namespace capra_2dnav {
+  class ClearOctomap : public nav_core::RecoveryBehavior {
+  public:
+    ClearOctomap() {}
+
+    void initialize(std::string name, tf::TransformListener* tf, costmap_2d::Costmap2DROS* global_costmap, costmap_2d::Costmap2DROS* local_costmap) {}
+
+    void runBehavior() {
+      ros::NodeHandle n;
+      auto client = n.serviceClient<std_srvs::Empty>("/octomap_server/reset");
+      auto srv = std_srvs::Empty();
+
+      if(client.call(srv)) {
+        ROS_INFO("Octomap cleared");
+      } else {
+        ROS_ERROR("Could not clear Octomap");
+      }
+    }
+
+
+  };
+
+  PLUGINLIB_EXPORT_CLASS(capra_2dnav::ClearOctomap, nav_core::RecoveryBehavior)
+}
